7767094276a6f987690fde32cba7905b2c6a6ea9
Łukasz <lukaszkogut1996@gmail.com>
2023-02-27 19:49:12 +0100

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,52 @@
 import { galleryItems } from './gallery-items.js'
-// Change code below this line
 
-// console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery')
 
-
-
-const imagesHTML = galleryItems.map((item) => {
+const imagesHTML = galleryItems.map(({ preview, original, description }) => {
 	return `<div class='gallery__item'>
-	<a class='gallery__link” href='${item.original}'>
-	<img class='gallery__image' src='${item.preview}' data-source='${item.original}' alt='${item.description}' />
-	 </a>
-	 </div>`;
+	  <a class='gallery__link' href='${original}'>
+		<img class='gallery__image' src='${preview}' data-source='${original}' alt='${description}' />
+	  </a>
+	</div>`;
   });
   gallery.insertAdjacentHTML('beforeend', imagesHTML.join(''));
 
-
-gallery.addEventListener('click', event => {
+  gallery.addEventListener('click', event => {
 	event.preventDefault()
 	if (event.target.tagName === 'IMG') {
 		const instance = basicLightbox.create(`
       <img src="${event.target.dataset.source}" alt="${event.target.alt}">
     `)
-		instance.show();
-		document.addEventListener('keydown', event => {
-			if (event.key === 'Escape') {
+
+    const onEscapePress = (event) => {
+      if (event.key === 'Escape') {
 			  instance.close();
+			  document.removeEventListener('keydown', onEscapePress);
 			}
-		  });
+    };
+		instance.show();
+		document.addEventListener('keydown', onEscapePress);
 	};
-})
+});
+
+
+// gallery.addEventListener('click', event => {
+// 	event.preventDefault()
+// 	if (event.target.tagName === 'IMG') {
+// 		const instance = basicLightbox.create(`
+//       <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+//     `)
+// 		instance.show();
+// 		document.addEventListener('keydown', event => {
+// 			if (event.key === 'Escape') {
+// 			  instance.close();
+// 			  document.removeEventListener('keydown', Escape);
+// 			}
+// 		  });
+// 		  document.addEventListener('keydown', Escape);
+// 	};
+// })
 
 
 // document.addEventListener('keydown', event => {
